0699e56fc55c5d09bd196e3b86dac94d37f96efa
Olivier Pinon <op@impero.com>
2019-09-06 01:15:29 +0200

    Formatting on example

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,18 +1,17 @@
-use iced::{Column, Cache, Text};
-use iced_pancurses::renderer::PancursesRenderer;
+use iced::{Cache, Column, Text};
+use iced_pancurses::PancursesRenderer;
 
 fn main() {
     let mut renderer = PancursesRenderer::default();
     let (view_y, view_x) = renderer.size();
-    let root: Column<(), PancursesRenderer> = 
-        Column::new()
+    let root: Column<(), PancursesRenderer> = Column::new()
         .width(view_x)
         .height(view_y)
         .spacing(1)
         .push(Text::new("Hello pancurses!\nThis is a toy renderer").color("blue"))
         .push(Text::new("Other text").color("red"));
-    let cache = Cache::default(); 
-    let ui = iced::UserInterface::build(root, cache, &renderer);  
+    let cache = Cache::default();
+    let ui = iced::UserInterface::build(root, cache, &renderer);
 
     ui.draw(&mut renderer);
     loop {
